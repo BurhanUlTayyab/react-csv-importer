5c437548d2387cc7018736d1b78827949a28acc0
BurhanUlTayyab <btayyab.bese15seecs@seecs.edu.pk>
2022-06-13 19:42:36 +0800

    Initial Commit

--- src/components/fields-step/ColumnDragTargetArea.tsx
+++ src/components/fields-step/ColumnDragTargetArea.tsx
@@ -99,8 +99,7 @@ const TargetBox: React.FC<{
   const [value, setValue] = useState(getInitialState);
   const handleChange = (e: any, fieldName: string, setField: any) => {
     setField(e.target.value);
-    onAssign(fieldName + "-" + e.target.value)
-    // onAssignChange(e, fieldName)
+    onAssignChange(e, fieldName)
     console.log("HANDLE_CHANGE", fieldName)
   };
   // @todo mouse cursor changes to reflect draggable state
