3487fa15d01f1514ba581f6bf8a947256efd5435
BurhanUlTayyab <btayyab.bese15seecs@seecs.edu.pk>
2022-06-13 21:41:38 +0800

    Initial Commit

--- src/components/fields-step/FieldsStep.tsx
+++ src/components/fields-step/FieldsStep.tsx
@@ -19,6 +19,7 @@ export type Field = DragField;
 
 export interface FieldsStepState {
   fieldAssignments: FieldAssignmentMap;
+  tagAssignments: TagAssignmentMap;
 }
 
 export const FieldsStep: React.FC<{
